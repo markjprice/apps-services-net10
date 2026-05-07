17caf255b4f060c1777612e1d8fd1eee00f41667
Mark J. Price <markjprice@gmail.com>
2026-05-07 09:26:41 +0100

    Add image converter

---
+++ code/ModernApps/Northwind.DesktopApp/CategoryIdToBitmapConverter.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data.Converters; // To use IValueConverter.
+using Avalonia.Media.Imaging; // To use Bitmap.
+using Avalonia.Platform; // To use AssetLoader.
+using System; // To use Uri and Type.
+using System.Globalization; // To use CultureInfo.
+
+namespace Northwind.DesktopApp.ViewModels
+{
+    internal class CategoryIdToBitmapConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+                if (value == null)
+
+                    return new Bitmap(AssetLoader.Open(new Uri($"avares://Northwind.DesktopApp/Assets/categories-small.jpeg")));
+
+                var categoryId = (int)value;
+
+                return new Bitmap(AssetLoader.Open(new Uri($"avares://Northwind.DesktopApp/Assets/category{categoryId}-small.jpeg")));
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
