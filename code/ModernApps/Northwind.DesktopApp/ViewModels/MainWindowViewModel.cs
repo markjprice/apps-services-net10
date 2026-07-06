a4ea764b4eddd97a58e54ff491777ee52590f86b
Mark J. Price <markjprice@gmail.com>
2026-07-06 09:01:31 +0100

    Fix category image binding

--- code/ModernApps/Northwind.DesktopApp/ViewModels/MainWindowViewModel.cs
+++ code/ModernApps/Northwind.DesktopApp/ViewModels/MainWindowViewModel.cs
@@ -1,57 +1,59 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Northwind.EntityModels; // To use Category and Product.
+﻿using Northwind.EntityModels; // To use Category and Product.
 using System.Collections.ObjectModel; // To use ObservableCollection<T>.
 using System.Linq; // To use LINQ methods.
 
 namespace Northwind.DesktopApp.ViewModels
 {
-  public partial class MainWindowViewModel : ViewModelBase
-  {
-    public string Greeting { get; } = "Welcome to Apps and Services with .NET 10 with Avalonia!";
-
-    public ObservableCollection<CategoryViewModel> Categories { get; } = [];
-    public ObservableCollection<Product> Products { get; } = [];
-
-    private Category? _selectedCategory;
-
-    public Category? SelectedCategory
+    public partial class MainWindowViewModel : ViewModelBase
     {
-      get => _selectedCategory;
-      set
-      {
-        _selectedCategory = value;
-        Products.Clear();
-        if (value != null)
+        public string Greeting { get; } = "Welcome to Apps and Services with .NET 10 with Avalonia!";
+
+        public ObservableCollection<CategoryViewModel> Categories { get; } = [];
+        public ObservableCollection<Product> Products { get; } = [];
+
+        private CategoryViewModel? _selectedCategory;
+
+        public CategoryViewModel? SelectedCategory
         {
-          foreach (var product in value.Products)
-            Products.Add(product);
+            get => _selectedCategory;
+            set
+            {
+                if (!SetProperty(ref _selectedCategory, value))
+                    return;
+
+                Products.Clear();
+
+                if (value != null)
+                {
+                    foreach (var product in value.Products)
+                        Products.Add(product);
+                }
+            }
         }
-      }
-    }
 
-    public MainWindowViewModel()
-    {
-      using NorthwindContext db = new();
-
-      CategoryViewModel[]? categories = db.Categories
-        .Select(c => new CategoryViewModel
+        public MainWindowViewModel()
         {
-          CategoryId = c.CategoryId,
-          CategoryName = c.CategoryName,
-          Description = c.Description,
-          Picture = c.Picture,
-          Products = c.Products
-        }).ToArray();
+            using NorthwindContext db = new();
 
-      if (categories is null)
-        return;
+            CategoryViewModel[]? categories = db.Categories
+              .Select(c => new CategoryViewModel
+              {
+                  CategoryId = c.CategoryId,
+                  CategoryName = c.CategoryName,
+                  Description = c.Description,
+                  Picture = c.Picture,
+                  Products = c.Products
+              }).ToArray();
 
-      foreach (CategoryViewModel category in categories)
-      {
-        Categories.Add(category);
-      }
+            if (categories is null)
+                return;
 
-      SelectedCategory = Categories[0];
+            foreach (CategoryViewModel category in categories)
+            {
+                Categories.Add(category);
+            }
+
+            SelectedCategory = Categories[0];
+        }
     }
-  }
 }
